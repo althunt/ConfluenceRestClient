1ab12cb62ffa9d87c63dd14474e5be0dbb1ca5b8
althunt <althunt@gmail.com>
2015-02-12 20:48:00 +0800

    Working console app that updates a single test page

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConfluenceClient.Confluence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Page page = new Page("http://localhost:8090", 557060);
+
+            page.body.storage.UpdateMacroContent("chart", "<table><tbody><tr><th>Hour</th><th>Errors</th></tr><tr><td>00</td><td>5</td></tr><tr><td>01</td><td>18</td></tr><tr><td>02</td><td>25</td></tr><tr><td>03</td><td>6</td></tr><tr><td>04</td><td>66</td></tr><tr><td>05</td><td>16</td></tr></tbody></table>");
+
+            page.Update();
+
+        }
+    }
+}
